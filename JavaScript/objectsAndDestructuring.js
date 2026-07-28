ab11097f309a8fea7e962484f7080e6376a3145f
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2026-07-28 13:56:04 -0300

    test: Destructuring propierties and turning them into variables, public ones

--- JavaScript/objectsAndDestructuring.js
+++ JavaScript/objectsAndDestructuring.js
@@ -5,4 +5,9 @@ const user = {
   age: 25,
   active: true,
 };
+//Acessing .name
 console.log(user.name);
+
+const { name, email, age, active } = user; //Unpacking and turning those those properties into variables of this scope
+
+console.log(name);
