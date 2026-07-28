4eaaa4fe462eba54385a3070b24d24675fc9f68f
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2026-07-28 11:35:34 -0300

    feat: compare normal functions to arrow ones

--- JavaScript/functionTypes.js
+++ JavaScript/functionTypes.js
@@ -1,7 +1,17 @@
-//Async//
+//Arrow Functions//
+function normalOne() {
+  console.log("this is the normal one");
+}
+let arrowTest = () => {
+  console.log("this is the arrow one");
+};
+/* Async
 async function one() {
   console.log("loading");
   await 2000;
   console.log(1);
 }
 one();
+*/
+normalOne();
+arrowTest();
