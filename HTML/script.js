869dd02f3dc59fa6265092ca58e24dd14fe4598d
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2026-07-30 20:57:14 -0300

    feat: create an fetch api wich shows random person in website

--- HTML/script.js
+++ HTML/script.js
@@ -10,13 +10,23 @@ function changeTitle(event) {
 async function getUser() {
   try {
     const response = await fetch("https://randomuser.me/api/");
-    const data = await response.json;
+    const data = await response.json();
     let randomNum = Math.floor(Math.random() * 100 + 1);
-    const user = data.results[randomNum];
-
-    console.log(user.picture.small);
-    console.log("${user.name.first} ${user.name.last}");
+    const user = data.results[0];
+    console.log(user);
+    console.log(user.picture.large);
+    console.log(`${user.name.first} ${user.name.last}`);
+    document.querySelector("#card").innerHTML = `
+        <img src = "${user.picture.large}"></img>
+<h3>${user.name.first} ${user.name.last}</h3> `;
   } catch (error) {
     console.log(error);
   }
 }
+let randomUserCard = document.querySelector("#card");
+let randomUserForm = document.querySelector("#randomFaceGeneratorForm");
+function getUserButton(event) {
+  event.preventDefault();
+  getUser();
+}
+randomUserForm.addEventListener("submit", getUserButton);
