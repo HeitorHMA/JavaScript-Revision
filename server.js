f4e945d1a4f8a335134dea315fab8eef825b8409
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2026-08-01 22:23:37 -0300

    feat:  post method learned using thunder client

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 2400;
-
+const productArray = ["bag", "ball", "doll", "videogame"];
+app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("TUNTUNTUN SAHUR");
-  const productArray = ["bag", "ball", "doll", "videogame"];
+  res.status(200).json(productArray);
+});
+
+app.post("/", (req, res) => {
+  const newProduct = req.body.name;
+  productArray.push(newProduct);
+  res.status(201).json(productArray);
+  console.log("posted");
 });
 
 app.listen(port, () => {
