2396a51781198d0140365c7fef2b70efeaaea308
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2026-08-02 04:02:29 -0300

    feat: learn method PUT in express.js

--- server.js
+++ server.js
@@ -7,11 +7,21 @@ app.get("/", (req, res) => {
   res.status(200).json(productArray);
 });
 
-app.post("/", (req, res) => {
+/*app.post("/", (req, res) => {
   const newProduct = req.body.name;
   productArray.push(newProduct);
   res.status(201).json(productArray);
   console.log("posted");
+});*/
+
+app.put("/:index", (req, res) => {
+  const index = req.params.index;
+  const newName = req.body.name;
+  productArray[index] = newName;
+  res.status(200).json(productArray);
+  console.log(
+    `Array Updated, element : ${newName} added to array at index : ${index}`,
+  );
 });
 
 app.listen(port, () => {
