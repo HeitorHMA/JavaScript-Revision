0932e36e46a9a78645560b6ad6467b1ab0a3cdce
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2026-08-03 13:45:57 -0300

    feat: create postgres file and add .env system for protect the personal info

---
+++ postgreSQL_test.js
@@ -0,0 +1,19 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error("not working", err.stack);
+  } else {
+    console.log("working");
+  }
+});
+
+module.exports = pool;
